10284819174c1b5810b378bb525efe67a87a2abf
Francesco Abbate <francesco.bbt@gmail.com>
2018-06-08 11:57:24 +0200

    Remove warning unsigned int narrowing

--- fox/window_fox.cpp
+++ fox/window_fox.cpp
@@ -21,11 +21,13 @@ void window_fox::update_region(graphics::image& src_img, const agg::rect_i& r) {
     const bool fximage_flipy = true;
     const int width = r.x2 - r.x1, height = r.y2 - r.y1;
 
-    debug_log("update_region: %d %d", width, height);
+    if (width <= 0 || height <= 0) return;
+
+    debug_log("update_region rect: %d %d %d %d", r.x1, r.y1, r.x2, r.y2);
 
     /* Create a rendering_buffer and the underlying memory buffer to
        store a FXColor array. */
-    image_gen<fximage_pixel_size, fximage_flipy> fxcolor_image{width, height};
+    image_gen<fximage_pixel_size, fximage_flipy> fxcolor_image{unsigned(width), unsigned(height)};
 
     rendering_buffer_ro src_view;
     rendering_buffer_get_const_view(src_view, src_img, r, graphics::image::pixel_size);
