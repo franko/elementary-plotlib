9e8e763546738d3ad92e24b4e0d9a31b748bb49e
Francesco Abbate <francesco.bbt@gmail.com>
2019-07-10 17:33:23 +0200

    Adapt test-window-function to use new public interface

--- tests/test-window-function.cpp
+++ tests/test-window-function.cpp
@@ -1,49 +1,44 @@
 #include <cmath>
 
-#include "window.h"
-#include "debug_log.h"
-#include "path.h"
+#include "libcanvas.h"
+
+using namespace libcanvas;
 
 template <typename Function>
-void add_function(graphics::plot& p, double x0, double x1, Function f, agg::rgba8 color, int n = 512) {
-    agg::rgba8 none(0,0,0,0);
-    auto line = new graphics::path();
-    line->move_to(x0, f(x0));
+void AddFunction(Plot& plot, double x0, double x1, Function f, Color color, int n = 512) {
+    Path line;
+    line.MoveTo(x0, f(x0));
     for (int i = 1; i <= n; i++) {
         const double x = x0 + i * (x1 - x0) / n;
-        line->line_to(x, f(x));
+        line.LineTo(x, f(x));
     }
-    p.add(line, color, 1.5, none, graphics::property::stroke);
+    plot.Add(line, color, 1.5, color::None, property::Stroke);
 }
 
-int main()
-{
-    graphics::initialize_fonts();
+int main() {
+    InitializeFonts();
 
-    graphics::window win;
-    graphics::plot p(graphics::plot::show_units | graphics::plot::auto_limits);
-    p.set_clip_mode(false);
-
-    agg::rgba8 blue(0, 0, 180, 255);
-    agg::rgba8 red(180, 0, 0, 255);
+    Window window;
+    Plot plot(Plot::ShowUnits | Plot::AutoLimits);
+    plot.SetClipMode(false);
 
     const double x0 = 0.0001, x1 = 8 * 2 *  3.14159265358979323846;
-    add_function(p, x0, x1, [](double x) { return std::sin(x) / x; }, blue);
+    AddFunction(plot, x0, x1, [](double x) { return std::sin(x) / x; }, color::Blue);
 
-    p.set_title("Function plot example");
-    p.set_x_axis_title("x variable");
+    plot.SetTitle("Function plot example");
+    plot.SetXAxisTitle("x variable");
 
-    p.commit_pending_draw();
+    plot.CommitPendingDraw();
 
-    int index = win.attach(&p, "");
+    int index = window.Attach(plot, "");
 
-    win.start(640, 480, graphics::window_resize);
+    window.Start(640, 480, WindowResize);
 
-    add_function(p, 0.8, x1, [](double x) { return std::cos(x) / x; }, red);
-    win.slot_refresh(index);
-    p.commit_pending_draw();
+    AddFunction(plot, 0.8, x1, [](double x) { return std::cos(x) / x; }, color::Red);
+    window.SlotRefresh(index);
+    plot.CommitPendingDraw();
 
-    win.wait();
+    window.Wait();
 
     return 0;
 }
