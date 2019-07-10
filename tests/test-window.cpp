32d44260791b3c566f0fb11ed0d3adcabcda9cc1
Francesco Abbate <francesco.bbt@gmail.com>
2019-07-10 23:29:39 +0200

    Declare using libcanvas in test-window

--- tests/test-window.cpp
+++ tests/test-window.cpp
@@ -1,27 +1,29 @@
 #include "debug_log.h"
 #include "libcanvas.h"
 
+using namespace libcanvas;;
+
 int main() {
-    libcanvas::InitializeFonts();
+    InitializeFonts();
 
-    libcanvas::Window window;
+    Window window;
 
-    libcanvas::Plot plot(libcanvas::Plot::ShowUnits); // TBD ShowUnits
+    Plot plot(Plot::ShowUnits);
     plot.SetLimits({-1.0, 0.0, 1.0, 10.0});
-    plot.SetAxisLabelsAngle(libcanvas::xAxis, 3.141592 / 4);
-    plot.EnableLabelFormat(libcanvas::xAxis, "%.6f");
+    plot.SetAxisLabelsAngle(xAxis, 3.141592 / 4);
+    plot.EnableLabelFormat(xAxis, "%.6f");
 
-    libcanvas::Polygon line{{-0.5, 0.0}, {-0.5, 8.0}, {0.5, 4.0}};
-    plot.Add(line, libcanvas::color::Red, 2.5, libcanvas::color::Yellow, libcanvas::property::Fill | libcanvas::property::Stroke);
+    Polygon line{{-0.5, 0.0}, {-0.5, 8.0}, {0.5, 4.0}};
+    plot.Add(line, color::Red, 2.5, color::Yellow, property::Fill | property::Stroke);
 
     plot.CommitPendingDraw();
     int index = window.Attach(plot, "");
 
-    window.Start(640, 480, libcanvas::WindowResize);
+    window.Start(640, 480, WindowResize);
     sleep(4);
 
-    libcanvas::Polygon line2{{0.8, 1.0}, {0.8, 7.0}, {0.3, 4.0}};
-    plot.Add(line2, libcanvas::color::Blue, 2.5, libcanvas::color::None);
+    Polygon line2{{0.8, 1.0}, {0.8, 7.0}, {0.3, 4.0}};
+    plot.Add(line2, color::Blue, 2.5, color::None);
 
     window.SlotRefresh(index);
     plot.CommitPendingDraw();
