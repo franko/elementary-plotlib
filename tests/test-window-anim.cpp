9d9b34e0b16afe5d7373230ec4e15287a44be972
Francesco Abbate <francesco.bbt@gmail.com>
2019-07-23 12:42:14 +0200

    Conversion in anumation test to public interface

--- tests/test-window-anim.cpp
+++ tests/test-window-anim.cpp
@@ -1,42 +1,40 @@
 #include <cmath>
 #include <random>
 
-#include "window.h"
 #include "debug_log.h"
-#include "path.h"
+#include "libcanvas.h"
 
-void rotate(const double a, double& x, double& y) {
+using namespace libcanvas;
+
+void Rotate(const double a, double& x, double& y) {
     double x_tmp = x * std::cos(a) + y * std::sin(a);
     y = -x * std::sin(a) + y * std::cos(a);
     x = x_tmp;
 }
 
-void
-add_triangle(graphics::plot& p, double x, double y, double size, double angle, agg::rgba8 fill_color) {
+void AddTriangle(Plot& plot, double x, double y, double size, double angle, Color fill_color) {
     double x1 = -size / 2, y1 = -size / (2 * std::sqrt(3.0));
     double x2 =  size / 2, y2 = -size / (2 * std::sqrt(3.0));
     double x3 = 0, y3 = size / std::sqrt(3.0);
 
-    rotate(angle, x1, y1);
-    rotate(angle, x2, y2);
-    rotate(angle, x3, y3);
+    Rotate(angle, x1, y1);
+    Rotate(angle, x2, y2);
+    Rotate(angle, x3, y3);
 
-    agg::rgba8 black(0, 0, 0, 255);
-    auto line = new graphics::polygon {{x + x1, y + y1}, {x + x2, y + y2}, {x + x3, y + y3}};
-    p.add(line, black, 1.0, fill_color, graphics::property::fill|graphics::property::stroke);
+    Polygon line{{x + x1, y + y1}, {x + x2, y + y2}, {x + x3, y + y3}};
+    plot.Add(line, color::Black, 1.0, fill_color, property::Fill | property::Stroke);
 }
 
-int main()
-{
-    graphics::initialize_fonts();
+int main() {
+    InitializeFonts();
 
-    graphics::window win("h.");
+    Window window("h.");
 
-    graphics::plot p(graphics::plot::show_units);
-    p.set_limits({0.0, 0.0, 100.0, 100.0});
-    int index = win.attach(&p, "1");
+    Plot plot(Plot::ShowUnits);
+    plot.SetLimits({0.0, 0.0, 100.0, 100.0});
+    int index = window.Attach(plot, "1");
 
-    win.start(640, 640, graphics::window_resize);
+    window.Start(640, 640, WindowResize);
 
     std::random_device r;
     std::default_random_engine e1(r());
@@ -46,20 +44,18 @@ int main()
     std::uniform_int_distribution<int> randi(0, 255);
 
     for (int k = 0; k < 30; k++) {
-        p.push_layer();
+        plot.PushLayer();
         for (int i = 0; i < 40; i++) {
             double x = unif_dist(e1), y = unif_dist(e1), size = size_dist(e1), angle = angle_dist(e1);
-            agg::rgba8 color(randi(e1), randi(e1), randi(e1), 255);
-            add_triangle(p, x, y, size, angle, color);
-            win.slot_refresh(index);
-            p.commit_pending_draw();
+            Color color = color::RGB(randi(e1), randi(e1), randi(e1));
+            AddTriangle(plot, x, y, size, angle, color);
+            window.SlotRefresh(index);
+            plot.CommitPendingDraw();
         }
         if (k+1 < 30) {
-            p.pop_layer();
+            plot.PopLayer();
         }
     }
-    printf("Test completed.\n");
-    win.wait();
-
+    window.Wait();
     return 0;
 }
