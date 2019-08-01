1cf21a2da46b9c2797e31f0dea1283bf54be623e
Francesco Abbate <francesco.bbt@gmail.com>
2019-08-01 12:32:12 +0200

    Rewrite graphics::path class to use directly Agg classes

--- include/path.h
+++ include/path.h
@@ -9,43 +9,60 @@
 
 namespace graphics {
 
-class path : public graphics::transform::scaling_a {
+class path : public sg_object {
 public:
-    path() : graphics::transform::scaling_a(&m_path), m_path() { }
+    path() : m_path(), m_scaling_matrix(), m_scaling_trans(m_path, m_scaling_matrix) { }
 
-    path(std::initializer_list<std::pair<double, double>> lst): graphics::transform::scaling_a(&m_path), m_path() {
+    path(std::initializer_list<std::pair<double, double>> lst): path() {
         bool first = true;
-        auto& p = m_path.self();
         for (const auto& v : lst) {
             if (first) {
-                p.move_to(v.first, v.second);
+                m_path.move_to(v.first, v.second);
                 first = false;
             } else {
-                p.line_to(v.first, v.second);
+                m_path.line_to(v.first, v.second);
             }
         }
     }
 
+    virtual void rewind(unsigned path_id) {
+        m_scaling_trans.rewind(path_id);
+    }
+
+    virtual unsigned vertex(double* x, double* y) {
+        return m_scaling_trans.vertex(x, y);
+    }
+
+    virtual void apply_transform(const agg::trans_affine& m, double as) {
+        m_scaling_matrix = m;
+    }
+
+    virtual void bounding_box(double *x1, double *y1, double *x2, double *y2) {
+        agg::bounding_rect_single(m_path, 0, x1, y1, x2, y2);
+    }
+
     void line_to(double x, double y) {
-        m_path.self().line_to(x, y);
+        m_path.line_to(x, y);
     }
 
     void move_to(double x, double y) {
-        m_path.self().move_to(x, y);
+        m_path.move_to(x, y);
     }
 
     void close_polygon() {
-        m_path.self().close_polygon();
+        m_path.close_polygon();
     }
 
     virtual sg_object *copy() const {
         path *new_object = new path();
-        m_path.copy_content(new_object->m_path);
+        vertex_source_copy(new_object->m_path, m_path);
         return new_object;
     }
 
 private:
-    sg_path m_path;
+    agg::path_storage m_path;
+    agg::trans_affine m_scaling_matrix;
+    agg::conv_transform<agg::path_storage> m_scaling_trans;
 };
 
 class polygon : public path {
