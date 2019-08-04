5b114b01803633265e24e136483b6beb7bcfead2
Francesco Abbate <francesco.bbt@gmail.com>
2019-08-04 12:22:38 +0200

    Port another test to public interface

--- tests/test-window-auto.cpp
+++ tests/test-window-auto.cpp
@@ -1,39 +1,31 @@
-#include "window.h"
 #include "debug_log.h"
-#include "path.h"
+#include "libcanvas.h"
 
-int main()
-{
-    graphics::initialize_fonts();
+using namespace libcanvas;;
 
-    graphics::window win("h.");
+int main() {
+    InitializeFonts();
 
-    graphics::plot p(graphics::plot::show_units | graphics::plot::auto_limits);
-    p.set_clip_mode(false);
+    Window window("h.");
 
-    agg::rgba8 none(0,0,0,0);
-    agg::rgba8 red(180, 0, 0, 255);
-    agg::rgba8 blue(0, 0, 180, 255);
-    agg::rgba8 yellow(245, 254, 0, 255);
+    Plot plot(Plot::ShowUnits | Plot::AutoLimits);
+    plot.SetClipMode(false);
 
-    auto line = new graphics::polygon {{-0.5, 0.0}, {-0.5, 8.0}, {0.5, 4.0}};
-    p.add(line, red, 2.5, yellow, graphics::property::fill | graphics::property::stroke);
+    Polygon line{{-0.5, 0.0}, {-0.5, 8.0}, {0.5, 4.0}};
+    plot.Add(line, color::Red, 2.5, color::Yellow, property::Fill | property::Stroke);
 
-    p.commit_pending_draw();
+    plot.CommitPendingDraw();
 
-    int index = win.attach(&p, "1");
-
-    win.start(640, 480, graphics::window_resize);
+    int index = window.Attach(plot, "1");
+    window.Start(640, 480, WindowResize);
 
     sleep(4);
 
-    auto line2 = new graphics::polygon {{0.8, 1.0}, {0.8, 7.0}, {0.3, 4.0}};
-    p.add(line2, blue, 2.5, none);
+    Polygon line2{{0.8, 1.0}, {0.8, 7.0}, {0.3, 4.0}};
+    plot.Add(line2, color::Blue, 2.5, color::None);
 
-    win.slot_refresh(index);
-    p.commit_pending_draw();
-
-    win.wait();
-
+    window.SlotRefresh(index);
+    plot.CommitPendingDraw();
+    window.Wait();
     return 0;
 }
