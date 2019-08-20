6598784252b7c9168d757dc5b982fecc3234f834
Francesco Abbate <francesco.bbt@gmail.com>
2019-08-20 17:08:52 +0200

    Simplify outline test
    Usage of unique_ptr no longer required because Plot implements now copy
    & move semantic.

--- tests/test-window-outline.cpp
+++ tests/test-window-outline.cpp
@@ -16,16 +16,16 @@ Object NewLineFunction(double x0, double x1, Function f, int n = 512) {
     return line;
 }
 
-std::unique_ptr<Plot> CreateFunctionPlot(const char *title, unsigned flags) {
-    auto p = std::make_unique<Plot>(Plot::ShowUnits | Plot::AutoLimits);
-    p->SetClipMode(false);
-    p->SetTitle(title);
+Plot CreateFunctionPlot(const char *title, unsigned flags) {
+    Plot p(Plot::ShowUnits | Plot::AutoLimits);
+    p.SetClipMode(false);
+    p.SetTitle(title);
     const double x0 = 3.14159265358979323846/2, x1 = 8 * 2 * 3.14159265358979323846;
     auto fsin = [](double x) { return std::sin(x) / x; };
     auto fcos = [](double x) { return std::cos(x) / x; };
-    p->Add(NewLineFunction(x0, x1, fsin), color::Blue, 1.5, color::None, flags);
-    p->Add(NewLineFunction(x0, x1, fcos), color::Red, 1.5, color::None, flags);
-    p->CommitPendingDraw();
+    p.Add(NewLineFunction(x0, x1, fsin), color::Blue, 1.5, color::None, flags);
+    p.Add(NewLineFunction(x0, x1, fcos), color::Red, 1.5, color::None, flags);
+    p.CommitPendingDraw();
     return p;
 }
 
@@ -34,13 +34,13 @@ int main() {
 
     Window win("v...");
 
-    auto plot1 = CreateFunctionPlot("Plot example (antialias)", property::Stroke);
-    auto plot2 = CreateFunctionPlot("Plot example (outline)", property::Outline);
-    auto plot3 = CreateFunctionPlot("Plot example (aliased)", property::Stroke | property::Crisp);
+    Plot plot1 = CreateFunctionPlot("Plot example (antialias)", property::Stroke);
+    Plot plot2 = CreateFunctionPlot("Plot example (outline)", property::Outline);
+    Plot plot3 = CreateFunctionPlot("Plot example (aliased)", property::Stroke | property::Crisp);
 
-    win.Attach(*plot1, "3");
-    win.Attach(*plot2, "2");
-    win.Attach(*plot3, "1");
+    win.Attach(plot1, "3");
+    win.Attach(plot2, "2");
+    win.Attach(plot3, "1");
 
     win.Start(480, 3 * 320, WindowResize);
     win.Wait();
