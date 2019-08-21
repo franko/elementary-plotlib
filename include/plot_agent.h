f1ab85cd9a6449d57dc122e75da12926707a9cd4
Francesco Abbate <francesco.bbt@gmail.com>
2019-08-21 16:50:44 +0200

    Use forward declaration for window in plot_agent
    More C++ idiomatic and type safe but still avoid bringing the whole
    window's header in plot_agent header file.

--- include/plot_agent.h
+++ include/plot_agent.h
@@ -4,13 +4,15 @@
 
 namespace graphics {
 
+class window;
+
 class plot_agent {
     struct window_index_pair {
-        void *window;
+        window *window;
         int slot_index;
     };
 public:
-    void add_window(void *w, int slot_index);
+    void add_window(window *w, int slot_index);
     void update_windows();
 private:
     agg::pod_bvector<window_index_pair> m_linked_windows;
