c81919bfd6b1842507bcf009426f52281d2833e8
Francesco Abbate <francesco.bbt@gmail.com>
2019-09-01 10:41:23 +0200

    Fix problem with arguments passed to stand-alone FOX window
    The variables argc, argv needs to be permanent because FXApp can access
    them also after initialization.

--- fox/FoxWindow.cpp
+++ fox/FoxWindow.cpp
@@ -24,8 +24,13 @@ private:
     FoxWindow(const FoxWindow&);
     FoxWindow &operator=(const FoxWindow&);
 public:
-    FoxWindow(int& argc, char **argv, const char *split_str) : app_("libcanvas", "libcanvas") {
-        app_.init(argc, argv);
+    FoxWindow(const char *split_str) : app_("libcanvas", "libcanvas") {
+        argc_ = 1;
+        strcpy(argv0_, "libcanvas");
+        argv_[0] = argv0_;
+        argv_[1] = nullptr;
+
+        app_.init(argc_, argv_);
         main_window_ = new FXMainWindow(&app_, "Graphics Window", nullptr, nullptr, DECOR_ALL, 0, 0, 0, 0);
         graphics_window_ = new GraphicsWindow(main_window_, split_str, LAYOUT_FILL_X|LAYOUT_FILL_Y);
         main_window_->setTarget(this);
@@ -61,7 +66,12 @@ public:
     }
 
     long onClose(FXObject *, FXSelector, void *);
+
 private:
+    int argc_;
+    char argv0_[16];
+    char *argv_[2];
+
     FXApp app_;
     FXMainWindow *main_window_;
     GraphicsWindow *graphics_window_;
@@ -82,19 +92,11 @@ long FoxWindow::onClose(FXObject *, FXSelector, void *) {
 }
 
 Window::Window() {
-    int argc = 1;
-    char argv0[32];
-    char *argv[] = {argv0, 0};
-    memcpy(argv0, "libcanvas", 16);
-    window_impl_ = (WindowImpl *) new FoxWindow(argc, argv, nullptr);
+    window_impl_ = (WindowImpl *) new FoxWindow(nullptr);
 }
 
 Window::Window(const char *split_str) {
-    int argc = 1;
-    char argv0[32];
-    char *argv[] = {argv0, 0};
-    memcpy(argv0, "libcanvas", 10);
-    window_impl_ = (WindowImpl *) new FoxWindow(argc, argv, split_str);
+    window_impl_ = (WindowImpl *) new FoxWindow(split_str);
 }
 
 Window::~Window() {
