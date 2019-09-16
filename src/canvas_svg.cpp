8a598247341a07982cf4d98a675d3c04e8f427d4
Francesco Abbate <francesco.bbt@gmail.com>
2019-09-16 16:00:28 +0200

    Remove debug message

--- src/canvas_svg.cpp
+++ src/canvas_svg.cpp
@@ -5,7 +5,6 @@ void canvas_svg::draw<sg_object>(sg_object& vs, agg::rgba8 c)
 {
     int id = m_current_id ++;
     str s = vs.write_svg(id, c, m_height);
-    fprintf(stderr, "canvas_svg::draw id: %d write_svg output: %s\n", id, s.cstr());
     canvas_svg::writeln(m_output, s, "   ");
 }
 
