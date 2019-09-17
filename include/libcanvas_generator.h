b03f23bbbd031b01b41f1017d465abd138649a28
Francesco Abbate <francesco.bbt@gmail.com>
2019-09-17 17:10:25 +0200

    Add type-generic generator functions

--- include/libcanvas_generator.h
+++ include/libcanvas_generator.h
@@ -3,14 +3,19 @@
 
 namespace libcanvas {
 
-template <typename Function>
-Path FxLine(double x0, double x1, Function f, int sampling_intervals = 128) {
-    Path line;
+template <typename Function, typename PathType = Path>
+void FxLineDraw(PathType& line, double x0, double x1, Function f, int sampling_intervals = 128) {
     line.MoveTo(x0, f(x0));
     for (int i = 1; i <= sampling_intervals; i++) {
         const double x = x0 + i * (x1 - x0) / sampling_intervals;
         line.LineTo(x, f(x));
     }
+}
+
+template <typename Function, typename PathType = Path>
+PathType FxLine(double x0, double x1, Function f, int sampling_intervals = 128) {
+    PathType line;
+    FxLineDraw(line, x0, x1, f, sampling_intervals);
     return line;
 }
 
