5faab4797c813f3c744fbc5dc457d5cceb2eb785
Francesco Abbate <francesco.bbt@gmail.com>
2020-01-10 14:22:59 +0100

    Move window_gen in a specific header
    The idea is that window_gen can be useful in other place in addition to
    the window native case. It permit the creation of class implementing the
    elp_window interface by bringing together the display_window and the
    window_surface.

--- include/window_platform_native.h
+++ include/window_platform_native.h
@@ -1,57 +1,6 @@
 #pragma once
 
-#include <thread>
-
-#include "window.h"
-#include "window_surface.h"
-#include "window_flags.h"
-#include "notify_request.h"
-#include "debug_priv.h"
-
-template <typename Window>
-class window_gen : public elp_window {
-public:
-    window_gen(): m_surface(nullptr), m_window(m_surface) {
-        m_surface.attach_window(&m_window);
-    }
-
-    window_gen(const char *fmt): m_surface(fmt), m_window(m_surface) {
-        m_surface.attach_window(&m_window);
-    }
-
-    void set_layout(const char *fmt) override {
-        if (m_window.status() == graphics::window_not_started) {
-            m_surface.split(fmt);
-        }
-    }
-
-    int attach(graphics::plot* p, const char* slot_str) override {
-        return m_surface.attach(p, slot_str);
-    }
-
-    void slot_refresh(unsigned index) override {
-        m_surface.slot_refresh(index);
-    }
-
-    void start(unsigned width, unsigned height, unsigned flags) override {
-        m_window.start(width, height, flags);
-    }
-
-    void wait() override {
-        request_error_e status = m_window.wait_until_notification(graphics::window_closed);
-        if (status != request_satisfied && status != request_success) {
-            debug_log(1, "error while waiting window's closing, error code: %d", int(status));
-        }
-    }
-
-    graphics::window_surface *get_window_surface() override {
-        return &m_surface;
-    }
-
-private:
-    graphics::window_surface m_surface;
-    Window m_window;
-};
+#include "window_gen.h"
 
 #if defined (WIN32) || defined (CYGWIN)
 #include "win32/window_win32.h"
