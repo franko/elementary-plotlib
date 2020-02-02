cd86e2f10e75d51375b8a6fbec9609c1eaece074
Francesco Abbate <francesco.bbt@gmail.com>
2020-02-02 23:06:48 +0100

    lua: complete sol2 declaration of C++ classes

--- lua/sol-test.cpp
+++ lua/sol-test.cpp
@@ -15,6 +15,27 @@ int main() {
         "ClosePolygon", &Path::ClosePolygon,
         sol::base_classes, sol::bases<Object>()
     );
+    elem.new_usertype<CurvePath>("CurvePath", sol::constructors<CurvePath()>(),
+        "MoveTo", &CurvePath::MoveTo,
+        "LineTo", &CurvePath::LineTo,
+        "Curve3", &CurvePath::Curve3,
+        "Curve4", &CurvePath::Curve4,
+        "ArcTo", &CurvePath::ArcTo,
+        "ClosePolygon", &CurvePath::ClosePolygon,
+        sol::base_classes, sol::bases<Object>()
+    );
+    elem.new_usertype<DashPath>("DashPath", sol::constructors<DashPath()>(),
+        "AddDash", &DashPath::AddDash,
+        sol::base_classes, sol::bases<Path, Object>()
+    );
+    elem.new_usertype<Markers>("Markers", sol::constructors<Markers(double, Object)>(),
+        sol::base_classes, sol::bases<Path, Object>()
+    );
+    elem.new_usertype<Text>("Text", sol::constructors<Text(const char*, double, double, double)>(),
+        "SetAngle", &Text::SetAngle,
+        "SetPosition", &Text::SetPosition,
+        sol::base_classes, sol::bases<Object>()
+    );
     elem.new_usertype<Plot>("Plot", sol::constructors<Plot(), Plot(unsigned)>(),
         "SetTitle", &Plot::SetTitle,
         "SetXAxisTitle", &Plot::SetXAxisTitle,
@@ -39,8 +60,11 @@ int main() {
         "Wait", &Window::Wait
     );
 
-    // Not yet done:
-    // enum { ShowUnits = 1 << 0, AutoLimits = 1 << 1, ClipRegion = 1 << 2 };
+    auto plot_flags = lua.create_table();
+    plot_flags["ShowUnits"] = Plot::ShowUnits;
+    plot_flags["AutoLimits"] = Plot::AutoLimits;
+    plot_flags["ClipRegion"] = Plot::ClipRegion;
+    elem["plot"] = plot_flags;
 
     elem["xAxis"] = xAxis;
     elem["yAxis"] = yAxis;
