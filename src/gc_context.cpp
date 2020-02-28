3fec9735d4c47bf6e0f1904fa935cc0814fca58b
Francesco Abbate <francesco.bbt@gmail.com>
2020-02-28 12:03:32 +0100

    Remove remaining debug print messages

--- src/gc_context.cpp
+++ src/gc_context.cpp
@@ -1,5 +1,3 @@
-#include <cstdio>
-
 #include "gc_context.h"
 #include "elem_plot_class.h"
 #include "window.h"
@@ -8,11 +6,9 @@ std::mutex global_window_gc_mutex;
 
 void gc_context::collect_visited() {
     for (unsigned i = 0; i < m_visited_windows.size(); i++) {
-        fprintf(stderr, "collect window: %p\n", m_visited_windows[i]); fflush(stderr);
         delete m_visited_windows[i];
     }
     for (unsigned i = 0; i < m_visited_plots.size(); i++) {
-        fprintf(stderr, "collect plot: %p\n", m_visited_plots[i]); fflush(stderr);
         delete m_visited_plots[i];
     }
 }
