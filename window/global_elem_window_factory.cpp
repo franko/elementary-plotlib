35432c9f4572219ba41cbe35f6235e6475052ac1
Francesco Abbate <francesco.bbt@gmail.com>
2020-11-09 11:57:33 +0100

    Fix Meson config for the SDL library
    It is important to use the method 'config-tool' for the SDL library
    otherwise weird linking error popup on Windows.

--- window/global_elem_window_factory.cpp
+++ window/global_elem_window_factory.cpp
@@ -1,9 +1,9 @@
 #include "global_elem_window_factory.h"
-#include "native_elem_window_factory.h"
-#include "sdl_elem_window_factory.h"
 
 #ifdef ELEM_PLOT_USE_SDL
+#include "sdl_elem_window_factory.h"
 elem_window_factory *global_elem_window_factory = new sdl_elem_window_factory{};
 #else
-elem_window_factory *global_elem_window_factory = new sdl_elem_window_factory{};
+#include "native_elem_window_factory.h"
+elem_window_factory *global_elem_window_factory = new native_elem_window_factory{};
 #endif
