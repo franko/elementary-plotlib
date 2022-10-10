c096cc7ee69dfcd70af12803d55e0285e153b1a8
Francesco Abbate <francesco.bbt@gmail.com>
2022-10-10 11:02:41 +0200

    Use only the SDL backend and remove others

--- window/global_elem_window_factory.cpp
+++ window/global_elem_window_factory.cpp
@@ -1,9 +1,4 @@
 #include "global_elem_window_factory.h"
 
-#ifdef ELEM_PLOT_USE_SDL
 #include "sdl_elem_window_factory.h"
 elem_window_factory *global_elem_window_factory = new sdl_elem_window_factory{};
-#else
-#include "native_elem_window_factory.h"
-elem_window_factory *global_elem_window_factory = new native_elem_window_factory{};
-#endif
